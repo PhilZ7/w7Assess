56754a6c8cea148590960410b2d0dc56fe66f558
PhilZ7 <phil84_@hotmail.com>
2021-10-15 12:32:13 -0500

    W7 assessment

---
+++ cs/sumFunction.js
@@ -0,0 +1,28 @@
+let myArr
+
+function addToZero([]) {
+    let i = this.i
+
+    
+    let sum = for ( 
+        
+    ) i += i 
+        if (sum === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+}
+
+addToZero([]);
+// -> False
+
+addToZero([1]);
+// -> False
+
+addToZero([1, 2, 3]);
+// -> False
+
+addToZero([1, 2, 3, -2]);
+// -> True
